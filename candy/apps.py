535354cee392ca3f24fb7c45bf1e27140436caf1
saedis-osk <saedis.osk.einarsdottir@gmail.com>
2023-05-01 15:50:09 +0000

    lagaði þetta

---
+++ candy/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CandyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'candy'
---
+++ candy/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from . import views
+    # http://localhost:8000/candies
+urlpatterns = [
+    path('', views.index, name="index"),
+]
