b8f79d8ce4c73e39f118264e8b2d734a14d7f3ec
hanna444 <hannap19@ru.is>
2023-05-03 09:56:13 +0000

    gagnastuff

--- pizza/settings.py
+++ pizza/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'candy-pizza-db',
+        'USER': 'candy-pizza-user',
+        'PASSWORD': 'Abc.12345',
+        'HOST': '34.88.199.104',
+        'PORT': '5432',
+
+
     }
 }
 
