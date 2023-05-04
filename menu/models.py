928a7ce78614a17233c337fe4a96df247aff42fb
hanna444 <hannap19@ru.is>
2023-05-04 13:16:59 +0000

    loksins komin med sma

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PizzaCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
+    category = models.ForeignKey(PizzaCategory, on_delete=models.CASCADE)
+    ingredient = models.CharField(max_length=255, blank=True)
+    size = models.CharField(max_length=255, blank=True)
+    toppings = models.CharField(max_length=255, blank=True)
+    price = models.FloatField()
+
+class PizzaImage(models.Model):
+    image = models.CharField(max_length=9999)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+
+
+class Drink(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
+    size = models.CharField(max_length=255, blank=True)
+    price = models.FloatField()
+
+class DrinkImage(models.Model):
+    image = models.CharField(max_length=9999)
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+
+
+
