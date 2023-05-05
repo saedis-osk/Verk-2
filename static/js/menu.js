84c3b387fe71070bf8d2d67330d4773608c0863e
saedis-osk <saedis.osk.einarsdottir@gmail.com>
2023-05-05 21:48:11 +0000

    search bar js

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,16 +1,35 @@
-// JavaScript code
-function search_pizza() {
-    let input = document.getElementById('searchbar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('pizza');
+function handleSearch(event) {
+  event.preventDefault(); // prevent the form from submitting and reloading the page
 
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";
-        }
+  const searchInput = document.querySelector('.search-input');
+  const searchQuery = searchInput.value.trim();
+
+  // Do something with the search query (e.g. fetch search results from a server)
+  function filterProducts(searchQuery) {
+  const productCards = document.querySelectorAll('.col.mb-5');
+
+  productCards.forEach((productCard) => {
+    const productName = productCard.querySelector('.fw-bolder').textContent.toLowerCase();
+
+    if (productName.includes(searchQuery.toLowerCase())) {
+      productCard.style.display = 'block';
+    } else {
+      productCard.style.display = 'none';
     }
+  });
 }
-Output:
+
+function handleSearch(event) {
+  event.preventDefault();
+
+  const searchInput = document.querySelector('.search-input');
+  const searchQuery = searchInput.value.trim();
+
+  filterProducts(searchQuery);
+}
+
+
+  console.log(`Search query: ${searchQuery}`);
+}
+
+// No need to add an event listener to the search button, as the form's onsubmit attribute takes care of it.
