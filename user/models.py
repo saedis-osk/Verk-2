7a5168b262c02ecf1dddc8839417e23e2dc443ef
eytorinn <eytoralex@gmail.com>
2023-05-08 13:14:11 +0000

    user profile added/ profile edit implementation

--- user/models.py
+++ user/models.py
@@ -4,11 +4,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-# class User(AbstractUser):
-#     phone_number = models.CharField(max_length=15, blank=True)
-#     address = models.CharField(max_length=255, blank=True)
-#     is_customer = models.BooleanField(default=False)
-#     is_employee = models.BooleanField(default=False)
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    is_customer = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.CharField(max_length=9999)
 
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+import user.forms.profile_form
+from user.models import Profile
 
 # Create your views here.
 def register(request):
@@ -13,3 +14,17 @@ def register(request):
     return render(request, 'user/register.html', {
         'form': UserCreationForm()
     })
+
+def profile(request):
+    profile = Profile.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            profile = form.save(commit=false)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    return render(request, 'user/profile.html', {
+        'form': ProfileForm(instance=profile)
+    })
+
