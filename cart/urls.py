206f9a0b0c1464dc52456adb0ea0c459f2097594
saedis-osk <saedis.osk.einarsdottir@gmail.com>
2023-05-09 15:25:56 +0000

    cart

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="cart-index"),
+    path('', views.index, name="confirmation"),
     path('<int:id>', views.get_cart_by_id, name="cart_details"),
     path('create_cart', views.create_cart, name="create_cart")
 ]
