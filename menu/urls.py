c789e2883fba5a38d8b1720db96454874e097c7f
saedis-osk <saedis.osk.einarsdottir@gmail.com>
2023-05-10 13:34:14 +0000

    bla

--- menu/urls.py
+++ menu/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-from .views import toppings_view
+#from .views import toppings_view
 from .views import drinks
 from .views import offers
 
@@ -15,7 +15,7 @@ urlpatterns = [
     path('create_pizza', views.create_pizza, name="create_pizza"),
     path('delete_pizza/<int:id>', views.delete_pizza, name="delete_pizza"),
     path('update_pizza/<int:id>', views.update_pizza, name="update_pizza"),
-    path('toppings/', toppings_view, name='toppings'),
+#    path('toppings/', toppings_view, name='toppings'),
     path('drinks/', views.drinks, name='drinks'),
     path('offers/', views.offers, name='offers'),
     path('update_pizza/<int:id>', views.update_pizza, name="update_pizza")
