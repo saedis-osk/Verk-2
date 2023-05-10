62a201c8d5d2934bfff25744ab7bf4643bf996d0
hanna444 <hannap19@ru.is>
2023-05-10 15:22:21 +0000

    rugl

--- menu/urls.py
+++ menu/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('<int:id>', views.get_pizza_by_id, name="pizza_details"),
     path('create_pizza', views.create_pizza, name="create_pizza"),
     path('delete_pizza/<int:id>', views.delete_pizza, name="delete_pizza"),
-    path('update_pizza/<int:id>', views.update_pizza, name="update_pizza")
+    path('update_pizza/<int:id>', views.update_pizza, name="update_pizza"),
+    path('menu', views.menu, name="menu"),
 ]
 # path('toppings/', toppings_view, name='toppings'),
 # path('drinks/', drinks_view, name='drinks'),
