7b4342ed177c7009c549d3e85bc044d60697faaa
saedis-osk <saedis.osk.einarsdottir@gmail.com>
2023-05-11 12:11:28 +0000

    toppings

--- menu/models.py
+++ menu/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
+
+
 # Create your models here.
 
 
 class Toppings(models.Model):
     name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
     description = models.CharField(max_length=255, blank=True)
     image = models.ImageField(upload_to='toppings/', default='/toppings/default.png')
     type = models.CharField(max_length=10)
+
 
 
     def __str__(self):
@@ -20,9 +24,10 @@ class Pizza(models.Model):
     category = models.CharField(max_length=10, null=True, blank=True)
     ingredient = models.CharField(max_length=255, blank=True)
     size = models.CharField(max_length=255, blank=True)
-    toppings = models.ForeignKey(Toppings, on_delete=models.CASCADE, default=Toppings)
+    #toppings = models.ForeignKey(models.ManyToManyField(Toppings), on_delete=models.CASCADE, default=Toppings)
     price = models.FloatField(default=0.0)
     image = models.ImageField(upload_to='pizza/', default='/pizza/default.png')
+    toppings = models.ManyToManyField(Toppings)
 
     def __str__(self):
         return self.name
