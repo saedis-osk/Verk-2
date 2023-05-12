230b73934d8fc9f51f4b810b84f76194894c2bce
eytorinn <eytoralex@gmail.com>
2023-05-12 10:23:50 +0000

    user is redirected to profile once logged in

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('register', views.register, name='register'),
-    path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('login', LoginView.as_view(next_page='profile', template_name='user/login.html'), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('profile', views.profile, name='profile')
 ]
